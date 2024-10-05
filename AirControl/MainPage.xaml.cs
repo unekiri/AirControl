d27ff3c69815f13de0c1346e6f809b48a2634f1b
unekiri <knpy0508@gmail.com>
2024-10-05 23:54:33 +0900

    feat: UIの実装

--- AirControl/MainPage.xaml.cs
+++ AirControl/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using AirControl.Publish;
+using AirControl.Subscribe;
 using Microsoft.Extensions.Configuration;
+using System.Text.Json;
 
 namespace AirControl
 {
@@ -7,41 +9,68 @@ namespace AirControl
     {
         public IConfiguration Configuration { get; private set; }
 
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        public MainPage(IConfiguration configuration)
+        public MainPage(IConfiguration configuration, SubscribedMessage subscribedMessage)
         {
             InitializeComponent();
             this.Configuration = configuration;
+            this.BindingContext = subscribedMessage;
         }
 
-        private async void SendMessageToMqttBroker(object sender, EventArgs e)
+        private async Task ConnectAndPublish(dynamic jsonPayload)
+        {
+            var address = this.Configuration["MqttSettings:Address"];
+            var port = int.Parse(this.Configuration["MqttSettings:Port"]);
+            var topic = this.Configuration["MqttSettings:Topic"];
+
+            var publisher = new Publisher();
+            // MQTTブローカーに接続
+            await publisher.ConnectToBroker(address, port);
+            // MQTTブローカーにメッセージを送信
+            await publisher.Publish(topic, jsonPayload);
+        }
+        
+        private async void SelectedValueSetting(object sender, EventArgs e)
         {
             try
             {
-                var address = this.Configuration["MqttSettings:Address"];
-                var port = int.Parse(this.Configuration["MqttSettings:Port"]);
-                var topic = this.Configuration["MqttSettings:Topic"];
+                // 選択された値を取得
+                var selectedType = typePicker.SelectedItem?.ToString();
+                var selectedTemperature = temperaturePicker.SelectedItem?.ToString();
+                var selectedAirflow = airflowPicker.SelectedItem?.ToString();
 
-                var publisher = new Publisher();
-                // MQTTブローカーに接続
-                await publisher.ConnectToBroker(address, port);
-                // MQTTブローカーにメッセージを送信
-                await publisher.Publish(topic);
+                // Json形式でpayloadを作成
+                var payload = new
+                {
+                    Type = selectedType,
+                    Temperature = selectedTemperature,
+                    Airflow = selectedAirflow
+                };
+
+                var jsonPayload = JsonSerializer.Serialize(payload);
+                await ConnectAndPublish(jsonPayload);
                 
-                await DisplayAlert("Success", "Connected to MQTT Broker and Sent a message", "OK");
-
+                await DisplayAlert("Success", "Completed the configuration", "OK");
             }
             catch (Exception ex)
             {
-                await DisplayAlert("Error", "Failed to connect or published a message to MQTT Broker: " + ex.Message, "OK");
+                await DisplayAlert("Error", "Failed to connect or published a message to MQTT Broker while setting: " + ex.Message, "OK");
+
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        private async void TurnOff(object sender, EventArgs e)
+        {
+            try
+            {
+                var jsonPayload = JsonSerializer.Serialize(new { });
+                await ConnectAndPublish(jsonPayload);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", "Failed to connect or published a message to MQTT Broker while turning off: " + ex.Message, "OK");
 
                 Console.WriteLine(ex.ToString());
             }
         }
     }
-
 }
---
+++ AirControl/Subscribe/SubscribedMessage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirControl.Subscribe
+{
+    public class SubscribedMessage : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Type { get; set; }
+        public string Temperature { get; set; }
+        public string Airflow { get; set; }
+        public void UpdateUI(string type, string temperature, string airflow)
+        {
+            this.Type = type;
+            this.Temperature = temperature;
+            this.Airflow = airflow;
+            OnPropertyChanged(nameof(Type));
+            OnPropertyChanged(nameof(Temperature));
+            OnPropertyChanged(nameof(Airflow));
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
